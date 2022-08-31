542cc50e046b285080b6f539bbdaa7d3e580a130
KewinEvers11 <kewin4@gmail.com>
2022-08-30 23:31:23 -0500

    adding graddle for kafka producer rest

---
+++ api-rest-producer/src/main/java/org/it/Test/Temp/TEST/ApiRestTemperatureProducer/ApiRestTemperatureProducerApplication.java
@@ -0,0 +1,13 @@
+package org.it.Test.Temp.TEST.ApiRestTemperatureProducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestTemperatureProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestTemperatureProducerApplication.class, args);
+	}
+
+}
